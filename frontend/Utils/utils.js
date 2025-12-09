52ad56590eba9eaa4cd3a30086ed2831391130ef
Tarundhote <161837594+Tarundhote@users.noreply.github.com>
2025-12-10 02:46:58 +0530

    Update BACKEND_URL to production server

--- frontend/Utils/utils.js
+++ frontend/Utils/utils.js
@@ -1,2 +1,2 @@
 // export const BACKEND_URL= "https://real-trust-website.onrender.com/api/v1"
-export const BACKEND_URL= "http://localhost:4000/api/v1"
+export const BACKEND_URL= "https://assingnment-p6yg.onrender.com/api/v1"
